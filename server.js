81c14cb5c80f77443d76cecdc9bbe37c7b534df6
Sunghun Park <sunghun.josephp@gmail.com>
2022-04-13 15:20:32 -0700

    Create Index route

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
+require('dotenv').config();
+
 // Dependencies
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 3001
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const budgets = require('./models/budget.js');
 
 
 // Routes
 // Get route
-app.get('/budgets')
+app.get('/budgets/', (req, res) => {
+	res.render('budgets_index.ejs', {budgets: budgets})
+});
 
 // New route
-app.get('/budgets/new/')
+app.get('/budgets/new/',);
 
 // Show route
-app.get('/budgets/:index/')
+app.get('/budgets/:index/',);
 
 // Create Route
-app.post('/budgets/')
+app.post('/budgets/',);
 //
 app.listen(PORT, () => {
-	console.log("We are listening on port ", PORT)
+	console.log(`We are listening on port ${PORT}`)
 })
